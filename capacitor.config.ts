cf4452c8dda66ba09eab9d5d10307ad25625ea5b
charlcinapaul-dotcom <charlcinapaul@gmail.com>
2026-03-13 16:21:15 -0400

    Update server URL and cleartext settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,8 +5,8 @@ const config: CapacitorConfig = {
   appName: 'LegacyFit',
   webDir: 'dist',
   server: {
-    url: 'https://02761c3c-61e0-4e61-9b90-3ed624c7a2e2.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
+    url: 'https://legacyfitvirtual.com',
+    cleartext: false,
   },
 };
 
